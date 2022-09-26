acf71ce58d58cbd0a0f60a9aef8b14da3d81dfa4
SergeiAndreev <sergeisergei2021@yandex.ru>
2022-09-26 19:02:43 +0300

    negative check

--- src/homework2/Interview.java
+++ src/homework2/Interview.java
@@ -2,10 +2,11 @@ package homework2;
 
 public class Interview {
     public static void main(String[] args) {
-        int age = 105;
-        int exp = 7;
-
-        if (age <100) {
+        int age = -105;
+        int exp = -7;
+        if (age < 0 ||  exp<0)
+            System.out.println("wrong age or / and exp");
+        else if (age <100 && age > 17) {
             if (exp < 5)
                 System.out.println("Trainee position available");
             else
--- src/homework2/SeasonNumber.java
+++ src/homework2/SeasonNumber.java
@@ -3,12 +3,13 @@ package homework2;
 public class SeasonNumber {
 
     public static void main(String[] args) {
-        int month = 8;
+        int month = 21;
 
         if (month>=3 && month <=5) System.out.println("Spring");
         else if (month>=6 && month <=8) System.out.println("Summer");
         else if (month>=9 && month <=11) System.out.println("Autumn");
         else if (month == 12 || month == 1 || month == 2) System.out.println("Winter");
+        else System.out.println("неверное число месяца");
 
 
     }
