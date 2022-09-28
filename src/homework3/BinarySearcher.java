d6e8340e1ba5ce3226471d4896371dd2ee689df6
SergeiAndreev <sergeisergei2021@yandex.ru>
2022-09-28 16:11:16 +0300

    Bsearch added

---
+++ src/homework3/BinarySearcher.java
@@ -0,0 +1,32 @@
+package homework3;
+import java.util.Scanner;
+public class BinarySearcher {
+    public static void main(String[] args) {
+        System.out.println("Загадайте  целое число от 2 до 100 включительно");
+        System.out.println("Попробую его отгадать");
+
+        Scanner scanner =  new Scanner(System.in);
+
+        int starter = 2;
+        int end = 100 ;
+        while (true){
+            int guess = starter + (end - starter)/2;
+            System.out.println("Ваше число  равно "+guess+" ?");
+             int answer = scanner.nextInt();
+             if (answer == 1){
+                 System.out.println("Угадал!! Игра окончена!!");
+                 break;}
+             else {
+                 System.out.println("Ваше число больше?");
+               answer = scanner.nextInt();
+               if (answer ==1)
+                  starter = guess;
+                else
+                    end = guess;
+
+             }
+
+        }
+
+    }
+}
