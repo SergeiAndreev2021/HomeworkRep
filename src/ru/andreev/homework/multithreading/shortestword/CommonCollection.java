ad8d44129bee396c1d3847f339faf9927852a55d
SergeiAndreev2021 <82467448+SergeiAndreev2021@users.noreply.github.com>
2022-11-19 17:30:25 +0300

    Delete CommonCollection.java

--- src/ru/andreev/homework/multithreading/shortestword/CommonCollection.java
+++
@@ -1,7 +0,0 @@
-package ru.andreev.homework.multithreading.shortestword;
-
-import java.util.concurrent.CopyOnWriteArrayList;
-
-public class CommonCollection {
-   public static CopyOnWriteArrayList<String> strings = new CopyOnWriteArrayList<>();
-}
